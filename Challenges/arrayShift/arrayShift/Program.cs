9c6996792873088450d6ffa37ffb23ecd62dbb4a
thejamilah <jamilahmcw@gmail.com>
2018-10-13 14:41:32 -0700

    insert comments into solution

--- Challenges/arrayShift/arrayShift/Program.cs
+++ Challenges/arrayShift/arrayShift/Program.cs
@@ -31,10 +31,18 @@ namespace arrayShift
 
         }
 
+        /// <summary>
+        /// Insert a number into the middle of an array
+        /// </summary>
+        /// <param name="array">Given array to insert number</param>
+        /// <param name="middleNum">Number to insert into array</param>
+        /// <returns></returns>
         public static int[] ArrayShift(int[] array, int middleNum)
         {
             int[] newArray = new int[array.Length + 1];
             int counter = 0;
+            //using decimal and Math.Ceiling allows the correct middle index to be located 
+            //with an unvenen index length
             decimal midIndex = Math.Ceiling((decimal)(array.Length) / 2);
             for (int i = 0; i < newArray.Length; i++)
             {
