254e71f061d924096713e311b19e089a3770f8f0
thejamilah <jamilahmcw@gmail.com>
2018-10-14 23:21:43 -0700

    beginning solution-attempting to swap

---
+++ Challenges/array_reverse/array_reverse/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace array_reverse
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            int[] inputArray = new int[] { 1, 2, 3, 4, 5 };
+
+            foreach (int i in inputArray)
+            {
+                Console.WriteLine(i);
+            }
+        }
+
+
+               
+
+    }
+
+
+}
