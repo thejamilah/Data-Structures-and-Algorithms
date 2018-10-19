9788137f5ed9534d62156459b6d6d38a25f353a3
thejamilah <jamilahmcw@gmail.com>
2018-10-19 10:22:32 -0700

    LL start

---
+++ Data-Structures/LinkedLists/LinkList/LinkList/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LinkList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
