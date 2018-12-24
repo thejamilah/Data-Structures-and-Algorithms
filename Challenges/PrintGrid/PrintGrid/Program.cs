37582bb3d026cda79e284a50e1cd86300b7b08b8
thejamilah <jamilahmcw@gmail.com>
2018-12-23 17:58:00 -0800

    Initial Commit

--- Challenges/PrintGrid/PrintGrid/Program.cs
+++ Challenges/PrintGrid/PrintGrid/Program.cs
@@ -2,11 +2,28 @@
 
 namespace PrintGrid
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Print Grid");
+            OutputGrid();
+
         }
+
+        public static void OutputGrid()
+        {
+            for (int r = 1; r <=5; r++)
+            {
+                for (int c = 1; c <= 5; c++)
+                {
+                    Console.Write(r * c);
+                }
+
+                Console.WriteLine();
+            }
+        }
+
     }
+
 }
