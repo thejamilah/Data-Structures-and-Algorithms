7f05ccc0ae1ffa263a37511f9f18fb48b3137962
thejamilah <jamilahmcw@gmail.com>
2018-12-27 09:24:54 -0800

    Reverse Linked List Started

--- Challenges/RepeatedWord/RepeatedWord/Program.cs
+++ Challenges/RepeatedWord/RepeatedWord/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 
 namespace RepeatedWord
 {
@@ -6,7 +7,51 @@ namespace RepeatedWord
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Repeted Word");
+            Console.WriteLine(" ");
+
+            string string1 = "The quick brown fox jumped over the moon";
+            
+            string string2 = "No rest for the wicked";
+
+            Console.WriteLine(string1);
+            Console.WriteLine($"The first word repeated is: {RepeatedWord(string1)}\n");
+
+            Console.WriteLine(string2);
+            Console.WriteLine($"The first repeated word is: {RepeatedWord(string2)}\n");
+
+            Console.WriteLine(" ");
+            Console.WriteLine("Repeated integers");
+
+            int[] array1 = { 2, 2, 3, 4, 4, 4, 5, 5, 7, 8, 8, 9 };
+
+            int[] array2 = { 2, 3, 4, 5, 6, 7, 8, 9 };
+
+            //Console.WriteLine(array1);
+            //Console.WriteLine($"The repeated integer is: {RepeatedInteger(array1)}\n");
+
+            //Console.WriteLine(array2);
+            //Console.WriteLine($"The repeated integer is: {RepeatedInteger(array2)}\n");
         }
+
+        public static string RepeatedWord(string input)
+        {
+            string[] temp = input.ToLower().Split(" ");
+            Hashtable table = new Hashtable();
+
+            foreach (string word in temp)
+
+                    if (!table.Contains(word))
+                {
+                    table.Add(word, word);
+                }
+                else
+                {
+                    return word;
+                }
+
+            return "There are no repeated words in this string";
+        }
+
     }
 }
